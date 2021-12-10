90ac14b97524ca4ae0ce6c203d7837d8c029b39d
Sai Manish <45118185+Sai-Manish@users.noreply.github.com>
2021-12-10 10:17:08 +0530

    Delete src directory

--- src/Free_RS_Entry.v
+++
@@ -1,7 +0,0 @@
-module Free_RRS_Entry (
-    input [1:0] RS_index
-);
-tomasulo.RS_Rtype_busy[RS_index] = 1'b0;
-tomasulo.RS_Rtype_valid[RS_index] = 1'b0;  
-tomasulo.need_to_stall = 1'b0;  
-endmodule
